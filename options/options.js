2812728c1cada00613a1fad06eacb88ae9993674
Crowdbotics <team@crowdbotics.com>
2023-06-30 16:35:35 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "idling-void-41827",
+  url: "https://idling-void-41827.botics.co",
+  api: "https://idling-void-41827.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
